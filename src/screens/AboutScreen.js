be82906aa0b6cea6e803f589ee3b4daa059fc227
DokiBrother <72850289+DokiBrother@users.noreply.github.com>
2021-09-06 17:09:13 +0700

    add About Screen

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,19 +1,37 @@
 import React from 'react';
-import {StyleSheet, View, Text, Dimensions, SafeAreaView} from 'react-native';
+import {StyleSheet, View, Text, Dimensions, SafeAreaView, Image, Pressable, Alert} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import ReturnScreen from '../components/ReturnScreen';
 
 const AboutScreen = ({navigation}) => {
+
+
   return (
-      <SafeAreaView style={styles.screenContainer}>
+      <ScrollView style={styles.screenContainer}>
           <ReturnScreen
                 title='About us'
                 screen='Setting'
                 navigation={navigation}
           />
           <View style={styles.bodyContainer}>
-
+          <Image
+            source={require('../images/logoclb.png')}
+            style={styles.logo}
+            resizeMode='cover'
+          />
+            <Image
+              resizeMode='cover'
+              style={styles.photo}
+              source={{uri: 'https://istech.vn/wp-content/uploads/2021/08/104706987_145759743767730_3012861106799817953_n-1-1.jpeg'}}
+            />
+            <Text style={styles.textStyle}>     ISTECH được thành lập từ các bạn sinh viên có đam mê về nghiên cứu khoa học nói chung, là các đại diện đến từ các ngành học khác nhau, từ đó, trong tương lai sẽ giúp CLB phát huy hết tiềm năng của các thành viên trong BTC, cũng như nắm bắt được xu hướng, mức độ quan tâm chung của sinh viên, thành viên tham gia ISTECH. CLB hoạt động dựa trên nguyên tắc tự nguyện tham gia của các thành viên, dân chủ trong hoạt động và quản lý tập trung.</Text>
+            <Pressable
+              onPress={() => Alert.alert('Under Development')}
+            >
+              <Text style={[styles.textStyle, {marginTop: 20}]}>Nhấn để tìm hiểu thêm</Text>
+            </Pressable>
           </View>
-      </SafeAreaView>
+      </ScrollView>
   );
 };
 
@@ -23,16 +41,37 @@ const styles = StyleSheet.create({
     backgroundColor: '#6360FF'
   },
   bodyContainer: {
-    flex: 3,
-    backgroundColor: '#F1F1FA',
-    borderTopStartRadius: 20,
-    borderTopEndRadius: 20,
-    paddingTop: 20
+    flex: 2,
+    backgroundColor: '#6360FF',
+    paddingTop: 20,
+    borderTopColor: 'white',
+    borderTopWidth: 0.3
   },
   partDivide: {
     fontSize: 15,
     color: 'gray',
     padding: 20,
+  },
+  logo: {
+    height: 100,
+    width: 200,
+    alignSelf: 'center',
+    marginTop: 10,
+  },
+  textStyle: {
+    paddingHorizontal: 40,
+    fontSize: 15,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  photo: {
+    height: 250,
+    width: Dimensions.get('window').width-70,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: 'white',
+    marginVertical: 20,
+    alignSelf: 'center'
   }
 });
 
