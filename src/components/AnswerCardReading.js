262906092d82356906d9ba4b9423230f2ee07e76
DokiBrother <72850289+DokiBrother@users.noreply.github.com>
2021-09-24 22:57:51 +0700

    Revert "Revert "Revert "Revert "Merge branch 'master' of https://github.com/mpdn99/english-learning-app""""
    This reverts commit 862c21afcafa535151148a3df76319fd580365d5.

--- src/components/AnswerCardReading.js
+++ src/components/AnswerCardReading.js
@@ -3,9 +3,26 @@ import { Alert, Modal, StyleSheet, Text, Pressable, View, Dimensions } from "rea
 import { SafeAreaView } from "react-native-safe-area-context";
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
-const AnswerCardReading = () => {
+const AnswerCardReading = ({value}) => {
 
   const [modalVisible, setModalVisible] = useState(false);
+  
+  const Questions = () => {
+    if (value == 1) {
+      return (
+        <View>
+          <Text>Hehe, here is the set of question 1</Text>
+        </View>
+      )
+    } else if (value == 2) {
+      return (
+        <View>
+          <Text>And here is question 2</Text>
+        </View>
+      )
+    }
+    return null;
+  }
 
   return (
     <SafeAreaView>
@@ -23,18 +40,18 @@ const AnswerCardReading = () => {
                 
                 <View style={styles.modalView}>
 
-                  <Pressable
-                    style={[styles.buttonClose]}
-                    onPress={() => setModalVisible(!modalVisible)}
-                    >
-                    <MaterialIcons name="expand-more" size={30} color="white" />
-                  </Pressable>
 
-                    {/* Content of Questions for chapter here */}
+                <Questions/>
 
-                    <Text style={styles.modalText}>Questions here</Text>
-                    
 
+                  <View style={{flexDirection: "column-reverse", flex: 1, alignItems: "center"}}>
+                    <Pressable
+                      style={[styles.buttonClose]}
+                      onPress={() => setModalVisible(!modalVisible)}
+                      >
+                      <MaterialIcons name="expand-more" size={30} color="white" />
+                    </Pressable>
+                  </View>
                 </View>
                 </View>
             </Modal>
@@ -67,12 +84,12 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.25,
     shadowRadius: 4,
-    elevation: 5
+    elevation: 5,
   },
   button: {
     borderRadius: 10,
     height: 70,
-    paddingHorizontal: 25,
+    paddingHorizontal: 40,
     marginRight: 20,
     marginLeft: -30,
   },
@@ -84,7 +101,7 @@ const styles = StyleSheet.create({
   },
   buttonClose: {
     backgroundColor: "#2196F3",
-    borderRadius: 30
+    borderRadius: 30,
   },
   textStyle: {
     color: "black",
@@ -92,7 +109,8 @@ const styles = StyleSheet.create({
   },
   modalText: {
     margin: 20,
-    textAlign: "center"
+    textAlign: "center",
+    
   }
 });
 
